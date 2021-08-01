fbfed5d7bdbab603c5c7bf84394a826c21af53fa
lass <hocinehoho53@gmail.com>
2021-08-01 05:45:20 +0100

    chat/comments

--- src/Chat.js
+++ src/Chat.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Chat() {
     return (
         <div className="chat">
-            
+            <h1>This is the Chat component !!</h1> 
+            <p>I leave this work for tomorrow 🪐</p>
         </div>
     )
 }
