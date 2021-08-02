c878a9879b47ee687cb5bb782a764f764f677d77
lass <hocinehoho53@gmail.com>
2021-08-02 02:03:27 +0100

    adding/chatHeaderComponent

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import './Chat.css'
+import ChatHeader from './ChatHeader'
 
 function Chat() {
     return (
         <div className="chat">
-
-                {/* remouve this Two line if you want to see app changing  */}
-           
-               <h1>This is the Chat component !!</h1> 
-               <p>I leave this work for tomorrow 🪐</p>
-            
+            <ChatHeader />
         </div>
     )
 }
