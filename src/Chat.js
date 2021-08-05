b34cfa91a8013ed55e99a45e53545eadbef55e02
lass <hocinehoho53@gmail.com>
2021-08-05 17:45:55 +0100

    Chat/component

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import './Chat.css'
 import ChatHeader from './ChatHeader'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
+
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader />
+            <div className="chat__messages">
+
+            </div>
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large"/>
+                <form>
+                    <input placeholder={"Messsage #TestChannel"} />
+                    <button className="chat__inputButton" type="submit">
+                        Send Message
+                    </button>
+                </form>
+                <div className="chat__inputIcon">
+                    <CardGiftcardIcon fontSize="large"/>
+                    <GifIcon fontSize="large"/>
+                    <EmojiEmotionsIcon fontSize="large"/>
+                </div>
+            </div>
+
         </div>
+
     )
 }
 
